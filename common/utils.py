3cb243800b90a8b36bc07a2aac0cfdfafcd9c7d8
Cpsr123 <111482236+Cpsr123@users.noreply.github.com>
2022-08-19 14:29:06 +0900

    Update utils.py

--- common/utils.py
+++ common/utils.py
@@ -268,7 +268,6 @@ def pearson(vector1, vector2):
     sum2_pow = sum([pow(v, 2.0) for v in vector2])
     #sum up the products
     p_sum = sum([vector1[i]*vector2[i] for i in range(n)])
-    #分子num，分母den
     num = p_sum - (sum1*sum2/n)
     den = math.sqrt((sum1_pow-pow(sum1, 2)/n)*(sum2_pow-pow(sum2, 2)/n))
     if den == 0:
